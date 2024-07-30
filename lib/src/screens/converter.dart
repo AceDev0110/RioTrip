22e60d1f02284de0f11c4ce7498d64aa457cb0f0
acedev0110 <rankace0110@gmail.com>
2024-07-30 22:02:51 +0900

    update convert and key

--- lib/src/screens/converter.dart
+++ lib/src/screens/converter.dart
@@ -42,30 +42,40 @@ class ConverterScreen extends StatelessWidget {
   // Function to build currency selection row
   Widget buildCurrencySelection(
       String selectedCurrency, void Function(String) onSelected) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Wrap(
+      spacing: 8.0,
+      runSpacing: 4.0,
       children: currencies.map((currency) {
         final isSelected = selectedCurrency == currency;
         return GestureDetector(
           onTap: () => onSelected(currency),
-          child: Row(
-            children: [
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/flags/$currency.png'),
-              ),
-              const SizedBox(width: 6),
-              Text(
-                currency,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: isSelected ? currencySelectionColor : textColor,
-                  fontWeight: isSelected ? FontWeight.w900 : FontWeight.w400,
-                  decoration: isSelected
-                      ? TextDecoration.underline
-                      : TextDecoration.none,
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 6.0),
+            decoration: BoxDecoration(
+              color: isSelected ? currencySelectionColor : Colors.transparent,
+              borderRadius: BorderRadius.circular(4.0),
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/flags/$currency.png'),
+                  radius: 12,
                 ),
-              ),
-            ],
+                const SizedBox(width: 6),
+                Text(
+                  currency,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: isSelected ? currencySelectionColor : textColor,
+                    fontWeight: isSelected ? FontWeight.w900 : FontWeight.w400,
+                    decoration: isSelected
+                        ? TextDecoration.underline
+                        : TextDecoration.none,
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       }).toList(),
@@ -75,7 +85,7 @@ class ConverterScreen extends StatelessWidget {
   Widget buildRateRow(String leftText, double rightValue, String fromCurrency,
       String toCurrency) {
     return Container(
-      padding: EdgeInsets.all(20),
+      padding: EdgeInsets.all(10),
       decoration: BoxDecoration(
         border: Border(
           bottom: BorderSide(
@@ -135,125 +145,126 @@ class ConverterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: currencySelectionColor,
-        flexibleSpace: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-            return Padding(
-              padding: EdgeInsets.only(
-                  top: MediaQuery.of(context)
-                      .padding
-                      .top), // This compensates for the status bar height
-              child: Stack(
-                children: [
-                  Center(
-                    child: Text(
-                      'Currency rates today',
-                      style: TextStyle(
-                        color: textColor,
-                        fontSize: 20.0,
+        appBar: AppBar(
+          backgroundColor: currencySelectionColor,
+          flexibleSpace: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              return Padding(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context)
+                        .padding
+                        .top), // This compensates for the status bar height
+                child: Stack(
+                  children: [
+                    Center(
+                      child: Text(
+                        'Currency rates today',
+                        style: TextStyle(
+                          color: textColor,
+                          fontSize: 20.0,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            );
-          },
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: FutureBuilder<Map<String, double>>(
-          future: fetchExchangeRates(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else {
-              final rates = snapshot.data!;
-
-              // Introduce local variables to track the selected currencies
-              String localFromCurrency = fromCurrency;
-              String localToCurrency = toCurrency;
-
-              return StatefulBuilder(
-                builder: (context, setState) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      buildRateList(rates),
-                      const SizedBox(height: 16),
-                      buildCurrencySelection(localFromCurrency, (currency) {
-                        setState(() {
-                          localFromCurrency = currency;
-                        });
-                      }),
-                      const SizedBox(height: 16),
-                      TextField(
-                        controller: amountController,
-                        style: TextStyle(color: Colors.white, fontSize: 16),
-                        decoration: InputDecoration(
-                          hintText: 'Amount to convert',
-                          hintStyle: TextStyle(color: Colors.white54),
-                          border: InputBorder.none,
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey),
-                          ),
-                          filled: true,
-                          fillColor: Color.fromRGBO(255, 255, 255, 0.1),
-                        ),
-                        keyboardType:
-                            TextInputType.numberWithOptions(decimal: true),
-                      ),
-                      const SizedBox(height: 16),
-                      Center(
-                        child: ElevatedButton(
-                          onPressed: () {
-                            fromCurrency = localFromCurrency;
-                            toCurrency = localToCurrency;
-                            convertCurrency(rates);
-                          },
-                          child: const Icon(Icons.swap_vert),
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      buildCurrencySelection(localToCurrency, (currency) {
-                        setState(() {
-                          localToCurrency = currency;
-                        });
-                      }),
-                      const SizedBox(height: 16),
-                      TextField(
-                        controller: resultController,
-                        readOnly: true,
-                        style: TextStyle(color: Colors.white, fontSize: 16),
-                        decoration: InputDecoration(
-                          hintText: 'Converted amount',
-                          hintStyle: TextStyle(color: Colors.white54),
-                          border: InputBorder.none,
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey),
-                          ),
-                          filled: true, // Enables background filling
-                          fillColor: Color.fromRGBO(
-                              255, 255, 255, 0.1), // Sets the background color
-                        ),
-                      ),
-                    ],
-                  );
-                },
+                  ],
+                ),
               );
-            }
-          },
+            },
+          ),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: FutureBuilder<Map<String, double>>(
+              future: fetchExchangeRates(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else {
+                  final rates = snapshot.data!;
+
+                  // Introduce local variables to track the selected currencies
+                  String localFromCurrency = fromCurrency;
+                  String localToCurrency = toCurrency;
+
+                  return StatefulBuilder(
+                    builder: (context, setState) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          buildRateList(rates),
+                          const SizedBox(height: 16),
+                          buildCurrencySelection(localFromCurrency, (currency) {
+                            setState(() {
+                              localFromCurrency = currency;
+                            });
+                          }),
+                          const SizedBox(height: 16),
+                          TextField(
+                            controller: amountController,
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                            decoration: InputDecoration(
+                              hintText: 'Amount to convert',
+                              hintStyle: TextStyle(color: Colors.white54),
+                              border: InputBorder.none,
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey),
+                              ),
+                              filled: true,
+                              fillColor: Color.fromRGBO(255, 255, 255, 0.1),
+                            ),
+                            keyboardType:
+                                TextInputType.numberWithOptions(decimal: true),
+                          ),
+                          const SizedBox(height: 16),
+                          Center(
+                            child: ElevatedButton(
+                              onPressed: () {
+                                fromCurrency = localFromCurrency;
+                                toCurrency = localToCurrency;
+                                convertCurrency(rates);
+                              },
+                              child: const Icon(Icons.swap_vert),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          buildCurrencySelection(localToCurrency, (currency) {
+                            setState(() {
+                              localToCurrency = currency;
+                            });
+                          }),
+                          const SizedBox(height: 16),
+                          TextField(
+                            controller: resultController,
+                            readOnly: true,
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                            decoration: InputDecoration(
+                              hintText: 'Converted amount',
+                              hintStyle: TextStyle(color: Colors.white54),
+                              border: InputBorder.none,
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey),
+                              ),
+                              filled: true, // Enables background filling
+                              fillColor: Color.fromRGBO(255, 255, 255,
+                                  0.1), // Sets the background color
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+        ));
   }
 }
